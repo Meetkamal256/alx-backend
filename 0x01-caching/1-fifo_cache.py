aaa3f957e192c542917b33d76da41a9634f62e40
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-08-02 03:06:15 +0100

    tasks 1-7

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -30,15 +30,15 @@ BaseCaching = __import__('base_caching').BaseCaching
 class FIFOCache(BaseCaching):
     """_summary_
     """
-
+    
     def __init__(self):
         """_summary_
         """
         super().__init__()
-
+    
     def put(self, key, item):
         """_summary_
-
+        
         Args:
                         key (_type_): _description_
                         item (_type_): _description_
@@ -51,12 +51,12 @@ class FIFOCache(BaseCaching):
                 first_key = next(iter(self.cache_data.keys()))
                 del self.cache_data[first_key]
                 print("DISCARD: {}". format(first_key))
-
+            
             self.cache_data[key] = item
-
+    
     def get(self, key):
         """return the value in self.cache_data linked to key
-
+        
         Args:
                         key (_type_): _description_
         """
